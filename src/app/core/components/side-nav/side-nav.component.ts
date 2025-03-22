4a1fa85ef3865e0b3c04acbea3193d0a06d9e5ee
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2025-03-22 13:34:22 +0000

    feat: enhance side navigation with dynamic menu items and improved styling

--- src/app/core/components/side-nav/side-nav.component.ts
+++ src/app/core/components/side-nav/side-nav.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+export type SideNavMenuConfiguration = {
+  title: string;
+  icon: string;
+  route: string;
+};
+
 @Component({
   selector: 'app-side-nav',
-  imports: [],
+  imports: [MatListModule, MatIconModule],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.scss'
 })
 export class SideNavComponent {
-
+  public readonly menuConfigurationItems: SideNavMenuConfiguration[] = [
+    {
+      title: 'Home',
+      icon: 'house',
+      route: '/',
+    },
+  ];
 }
